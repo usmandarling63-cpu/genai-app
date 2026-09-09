e6a7ab22f5ccf8611be9053083a7bf24304e3975
Usman <usmandarling63@gmail.com>
2026-09-09 13:14:42 +0530

    Fix translation app

--- app.py
+++ app.py
@@ -10,26 +10,23 @@ webhook_url = "https://shaik5195522.app.n8n.cloud/webhook/daefa7c5-372a-4a0b-a69
 if st.button("Hindi", key="Hindi_btn"):
     if text:
         response = requests.post(
-            webhook_url,
+            url=webhook_url,
             json={"input": text, "language": "Hindi"}
         )
 
         if response.status_code == 200:
-    st.write("Response from n8n:")
-    st.write(response.json())
-else:
-    st.error(f"Translation failed: {response.status_code}")
-    st.write(response.text)
+            st.write(response.json()[0]["text"])
+        else:
+            st.error(f"Error: {response.status_code}")
+
 elif st.button("Telugu", key="Telugu_btn"):
     if text:
         response = requests.post(
-            webhook_url,
+            url=webhook_url,
             json={"input": text, "language": "Telugu"}
         )
 
         if response.status_code == 200:
-    st.write("Response from n8n:")
-    st.write(response.json())
-else:
-    st.error(f"Translation failed: {response.status_code}")
-    st.write(response.text)
+            st.write(response.json()[0]["text"])
+        else:
+            st.error(f"Error: {response.status_code}")
